5e26d7492ef0339381b5cff339721be4c2706d64
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2018-07-22 14:02:45 -0700

    Update CognitiveServicesConstants.cs

--- TextMood/TextMood/Constants/CognitiveServicesConstants.cs
+++ TextMood/TextMood/Constants/CognitiveServicesConstants.cs
@@ -8,7 +8,7 @@ namespace TextMood
         public const string TextSentimentAPIKey = "Your API Key";
 
 #error Missing Endpoint for the Cognitive Servce.You can locate the Azure Region on the Overview page of the TextAnalytics resource created in the Azure Portal.
-        const string _cognitiveServicesEndpoint = "Your Cognitive Services Enpoint";
+        const string _cognitiveServicesEndpoint = "Your Cognitive Services Endpoint";
 
         public static Uri BaseUri => new Uri(_cognitiveServicesEndpoint);
     }
