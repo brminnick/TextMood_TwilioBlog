e502a2bb3d43d4c2113e83e0607c4dcb28c1d803
Brandon Minnick <brminnick@users.noreply.github.com>
2018-07-22 14:00:40 -0700

    Updated Constants

--- TextMood/TextMood/Constants/CognitiveServicesConstants.cs
+++ TextMood/TextMood/Constants/CognitiveServicesConstants.cs
@@ -7,9 +7,9 @@ namespace TextMood
 #error Missing API Key. Create an API Key at https://aka.ms/cognitive-services-text-analytics-api
         public const string TextSentimentAPIKey = "Your API Key";
 
-#error Missing Azure Region for BaseUri, e.g. "westus". You can locate the Azure Region on the page of the TextAnalytics resource created in the Azure Portal.
-        const string _azureRegion = "Text Analytics Azure Region";
+#error Missing Endpoint for the Cognitive Servce.You can locate the Azure Region on the Overview page of the TextAnalytics resource created in the Azure Portal.
+        const string _cognitiveServicesEndpoint = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0";
 
-        public static Uri BaseUri => new Uri($"https://{_azureRegion}.api.cognitive.microsoft.com/text/analytics/v2.0");
+        public static Uri BaseUri => new Uri(_cognitiveServicesEndpoint);
     }
 }
